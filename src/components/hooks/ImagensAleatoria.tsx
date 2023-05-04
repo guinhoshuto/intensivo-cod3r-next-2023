212b0c8adbb61a0c986acf0149ca15898a7ddea6
Guilherme Shuto <guinho.shuto@gmail.com>
2023-05-04 17:25:44 -0400

    aula 2

--- src/components/hooks/ImagensAleatoria.tsx
+++ src/components/hooks/ImagensAleatoria.tsx
@@ -2,11 +2,12 @@ import Image from "next/image";
 import { useState } from "react";
 
 export default function ImagemAleatoria(){
-    const url = 'https://source.unsplash.com/featured/300x300?'
+    const url = 'https://source.unsplash.com/featured/'
     const [pesquisa, setPesquisa] = useState<string>('abstract')
+    const [size, setSize] = useState<number>(300)
 
     function urlImagem(){
-        return `${url}${pesquisa}`
+        return `${url}${size}x${size}?${pesquisa}`
     }
 
     function buttonRender(valor: string){
@@ -22,11 +23,25 @@ export default function ImagemAleatoria(){
 
     return (
         <div>
-            <Image src={urlImagem()} height="300" width="300" alt="x"/>
+            <div className="flex justify-center gap-7 mb-5">
+                <span>
+                    {pesquisa} - {size}x{size}
+                </span>
+            </div>
+            <Image className="rounded-md" src={urlImagem()} height="300" width="300" alt="x"/>
             <div className="flex flex-col gap-3 mt-3">
                 {buttonRender('abstract')}
                 {buttonRender('city')}
                 {buttonRender('nature')}
+            </div>
+            <div>
+                <input 
+                    type="number" 
+                    value={size}  
+                    className="bg-zinc-800 mr-3 mt-3 w-[90%] p-2 rounded-md outline-none"
+                    onChange={e => setSize(Number(e.target.value))}
+                />
+                <span>px</span>
             </div>
         </div>
     )
